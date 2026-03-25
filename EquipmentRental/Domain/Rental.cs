9854b6f38c4a1e9befba4a3b2813a737a2392082
Krzysztof Lisowski <s34072@pjwstk.edu.pl>
2026-03-25 17:32:16 +0100

    Feature: Dodanie modelu Rental obsługującego wypożyczenia i zwroty

---
+++ EquipmentRental/Domain/Rental.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace EquipmentRental.Domain;
+
+public class Rental
+{
+    public Guid Id { get; }
+    public User User { get; }
+    public Equipment Equipment { get; }
+    public DateTime RentalDate { get; }
+    public DateTime DueDate { get; }
+    public DateTime? ReturnDate { get; private set; } 
+    
+    public bool IsOverdue => !ReturnDate.HasValue && DateTime.Now > DueDate;
+
+    public Rental(User user, Equipment equipment, int rentalDays)
+    {
+        Id = Guid.NewGuid();
+        User = user ?? throw new ArgumentNullException(nameof(user));
+        Equipment = equipment ?? throw new ArgumentNullException(nameof(equipment));
+        
+        RentalDate = DateTime.Now;
+        DueDate = RentalDate.AddDays(rentalDays);
+    }
+
+    public void ReturnEquipment()
+    {
+        if (ReturnDate.HasValue)
+        {
+            throw new InvalidOperationException("Ten sprzęt został już zwrócony.");
+        }
+
+        ReturnDate = DateTime.Now;
+    }
+}
